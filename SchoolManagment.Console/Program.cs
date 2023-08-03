5543a3be24ef05ceec4e0df1c36aaca06d64e065
Abdurabu Gubran <abumoaadg@hotmail.com>
2023-08-03 20:38:04 +0300

    w3 school code

--- SchoolManagment.Console/Program.cs
+++ SchoolManagment.Console/Program.cs
@@ -1,9 +1,104 @@
-﻿
-using System;
-    internal class Program
+﻿using System;
+
+namespace SchoolManagment.Console
+{
+   
+    // fName =Ahmed, sName=Mohammed, faName= Gubran => fName+" "+sName+" "+faName Ahmed Mohammed Gubran
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // C# Comments    ctrl+c 
+            //    UnComments  ctrl+u
+            // /*   */
+            //System.Console.WriteLine("Hello World!");
+            //System.Console.WriteLine("I am Learning C#");
+            //System.Console.WriteLine("It is awesome!");
+            //System.Console.WriteLine(3 + 3);
+            //System.Console.WriteLine("Enter X value : ");
+            //int x = Convert.ToInt32(System.Console.ReadLine());
+
+            //System.Console.WriteLine("Enter Y value : ");
+            //int y = Convert.ToInt32(System.Console.ReadLine());
+            //System.Console.WriteLine("X+Y=" + (x + y));  //string.Concat("X+Y =", (x + y)));
+
+            //variables : 
+            //int myVar;
+            //myVar = 5;
+            //double myDoubleNum = 5.99;
+            //char myLetter = 'A';
+            //bool myBool = true;
+            //string myText = "Hello";
+            //System.Console.Write("value : " + myVar + "\n");
+            //System.Console.Write("value : " + myDoubleNum + "\n");
+            //System.Console.Write("value : " + myLetter + "\n");
+            //System.Console.Write("value : " + myBool + "\n");
+            //System.Console.Write("value : " + myText + "\n");
+
+
+            //Constant 
+            //const int myNum=15 ; // the value must be write in development mode and can not change in runtime mode 
+            //myNum = 20; // error
+            //Implicit Casting -> from small number to large number - no nead will do it automatically
+            //Explicit Casting -> from large to small - need to specify 
+            //double x = 214;
+
+            //int y = (int)x;
+
+
+            //string status = "false";
+            //bool statusInBool =Convert.ToBoolean(status);
+
+            // i++ => i=i+1
+            // i-- => i=i-1
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    System.Console.Write("when use i ++  : " + i + "\n");
+            //}
+            //for (int i = 10; i > 0; i--)
+            //{
+            //    System.Console.Write("when use i -- : " + i + "\n");
+            //}
+
+            //int x = 10;
+            ////x += 5; // x = x+5
+            ////&& and 
+            ////|| or 
+            //if (x==5 || y==3)
+            //{
+
+            //}
+
+
+            //int x = 10;
+            //System.Console.WriteLine("Math.Pow(x, 2)  : " + Math.Pow(x, 2));
+
+            int x = 3;
+            switch (x)
+            {
+                case 0:
+                    System.Console.WriteLine($"X is equal 0"); 
+                    break;
+                case 1:
+                    System.Console.WriteLine($"X is equal 1");
+                    break;
+                case 2:
+                    System.Console.WriteLine($"X is equal 2");
+                    break;
+
+                default:
+                    System.Console.WriteLine($"X not in my ranage");
+                    break;
+            }
+
+            //string fName = "Ziad";
+            //string sName = "Mubarak";
+            //int age = 21;
+            //System.Console.WriteLine($"Full Name \" {fName[3]} - {sName} and his age is {age} ");
         }
     }
+}
+
+
+// Database -> SqlClient
+// Network  -> Socket TCP UDP 
